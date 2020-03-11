262456dd76c7224178d18813b6be85a4d26e94d9
Raul Brumar <raulbrumar@yahoo.de>
2020-03-11 23:31:18 +0200

    Search working properly

--- Marketplace/Screens/HomeScreen.js
+++ Marketplace/Screens/HomeScreen.js
@@ -51,12 +51,74 @@ export default class HomeScreen extends Component
       });
     }
 
+    searchByLocation = (searchValue) =>
+    {
+      console.log('getting items by location');
+      fetch('https://marketplaceapialexraul.azurewebsites.net/search/location/' + searchValue, {
+        method: 'GET'
+      })
+      .then(response => {
+        if (response.ok == false) {
+          throw new Error("HTTP Code " + response.status + " - " + JSON.stringify(response.json()));
+        }
+        return response.json();
+      })
+      .then(json => {
+        console.log("Items by location GET successful")
+        console.log("Received following JSON");
+
+        this.setState({items: json}, function() {
+          console.log(this.state.items);
+        });
+      })
+      .catch(error => {
+        console.log("Error message:")
+        console.log(error.message)
+      });
+    }
+
+    searchByDate = (searchValue) =>
+    {
+      console.log('getting items by date');
+      fetch('https://marketplaceapialexraul.azurewebsites.net/search/date/' + searchValue, {
+        method: 'GET'
+      })
+      .then(response => {
+        if (response.ok == false) {
+          throw new Error("HTTP Code " + response.status + " - " + JSON.stringify(response.json()));
+        }
+        return response.json();
+      })
+      .then(json => {
+        console.log("Items by date GET successful")
+        console.log("Received following JSON");
+
+        this.setState({items: json}, function() {
+          console.log(this.state.items);
+        });
+      })
+      .catch(error => {
+        console.log("Error message:")
+        console.log(error.message)
+      });
+    }
+
+    searchLogic = (searchValue) =>
+    {
+      if (this.state.radioButton == 0)
+        this.searchByCategory(searchValue);
+      else if (this.state.radioButton == 1)
+        this.searchByLocation(searchValue);
+      else (this.state.radioButton == 2)
+        this.searchByDate(searchValue);
+    }
+
     render(){
       return (
         <View style={styles.container}>
           <StatusBar hidden={ true}></StatusBar>
           <Header height={ 30 } button1={'Login'} button2={'Post'} title={'Marketplace'} navigation={this.props.navigation}></Header>
-          <Searchbar search= {this.searchByCategory}></Searchbar>
+          <Searchbar search= {this.searchLogic}></Searchbar>
           <RadioForm
             style= { styles.radioButton }
             radio_props= { radio_props }
